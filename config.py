ad6203d519477d2e0a6eb20a37904108cca52491
Rolando Rabines <MugMug@Rolandos-MacBook-Pro.local>
2016-06-27 12:24:06 -0400

    first commit - no flask folder included

---
+++ config.py
@@ -0,0 +1,9 @@
+WTF_CSRF_ENABLED  = True
+SECRET_KEY = 'UseprimeNumbersforEvenDistribution'
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
